0157f78bf0835ed1ffca51587bf2cc246a1089b7
Italo Braga <102165012+italo71@users.noreply.github.com>
2022-10-31 15:58:17 -0300

    Add files via upload

---
+++ 1/1/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1
+{
+    public class Aluno
+    {
+        public string Nome;
+        public string Mail;
+        public int Telefone;
+        public Data DataNasc;
+    }
+    public class Data
+    {
+        public string Dia;
+        public string Mês;
+        public string Ano;
+    }
+    class Program
+    {
+        static void ExibeAlunos(Aluno[] aluno)
+        {
+            foreach (Aluno x in aluno)
+            {
+                Console.WriteLine($"{x.Nome} - Nasceu em {x.DataNasc.Dia} de {x.DataNasc.Mês} de {x.DataNasc.Ano}");
+                Console.WriteLine($"Seu telefone é {x.Telefone} e seu email é {x.Mail}");
+            }
+        }
+        static void Main(string[] args)
+        {
+
+            Aluno[] aluno = new Aluno[50];
+
+            for (int i = 0; i < 50; i++)
+            {
+                Aluno x = new Aluno();
+
+                Console.WriteLine("Nome do aluno:");
+                x.Nome = Console.ReadLine();
+
+                Console.WriteLine("Email do aluno:");
+                x.Mail = Console.ReadLine();
+
+                x.DataNasc = new Data();
+
+                Console.Write("Data de Nascimento - Dia: ");
+                x.DataNasc.Dia = Console.ReadLine();
+
+                Console.Write("                     Mês: ");
+                x.DataNasc.Mês = Console.ReadLine();
+
+                Console.Write("                     Ano: ");
+                x.DataNasc.Ano = Console.ReadLine();
+
+                Console.WriteLine("Telefone do aluno:");
+                x.Telefone = int.Parse(Console.ReadLine());
+
+                aluno[i] = x;
+            }
+
+            ExibeAlunos(aluno);
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
